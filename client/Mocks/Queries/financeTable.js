d35471ad9d52bffd7b084299ecf10fa23e80a279
yan <ian24257587@gmail.com>
2019-09-03 23:38:58 +0800

    add finance table header query mock

---
+++ client/Mocks/Queries/financeTable.js
@@ -0,0 +1,127 @@
+// @flow
+
+// type: chip
+
+export const headerData = [{ // query: chips
+  id: 1,
+  name: '資產',
+  layerNumber: 1,
+  childNodes: [{ // type: [chip]
+    id: 2,
+    name: '現金及約當現金',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 3,
+    name: '短期投資',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 4,
+    name: '應收帳款',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 5,
+    name: '存貨',
+    layerNumber: 2,
+    childNodes: [],
+  }],
+}, {
+  id: 6,
+  name: '流動資產',
+  layerNumber: 1,
+  childNodes: [{
+    id: 7,
+    name: '長期投資',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 8,
+    name: '固定資產',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 9,
+    name: '其他資產',
+    layerNumber: 2,
+    childNodes: [],
+  }],
+}, {
+  id: 10,
+  name: '非流動資產',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 11,
+  name: '資產總額',
+  layerNumber: 1,
+  childnode: [],
+}, {
+  id: 12,
+  name: '負債',
+  layerNumber: 1,
+  childnode: [{
+    id: 13,
+    name: '短期借款',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 14,
+    name: '應付帳款',
+    layerNumber: 2,
+    childNodes: [],
+  }],
+}, {
+  id: 15,
+  name: '流動負債',
+  layerNumber: 1,
+  childNodes: [{
+    id: 16,
+    name: '長期負債',
+    layerNumber: 2,
+    childNodes: [],
+  }, {
+    id: 17,
+    name: '其他負債',
+    layerNumber: 2,
+    childNodes: [],
+  }],
+}, {
+  id: 18,
+  name: '非流動負債',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 19,
+  name: '負債總額',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 20,
+  name: '歸屬於母公司業主之權益',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 21,
+  name: '股本合計',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 22,
+  name: '資本公積合計',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 23,
+  name: '保留盈餘合計',
+  layerNumber: 1,
+  childNodes: [],
+}, {
+  id: 24,
+  name: '權益總額',
+  layerNumber: 1,
+  childNodes: [],
+}];
+
+export default null;
