8773a612bc66176697405d8eaa6bfea9cea982c6
yan <ian24257587@gmail.com>
2019-09-04 00:18:45 +0800

    add store.js

--- client/App.jsx
+++ client/App.jsx
@@ -1,11 +1,29 @@
 // @flow
 
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import store, { history } from './store';
+
+const styles = {
+  wrapper: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    flexDirection: 'column',
+  },
+};
 
 function App() {
   return (
-    <div>
-      App
+    <div style={styles.wrapper}>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <div>aa</div>
+        </ConnectedRouter>
+      </Provider>
     </div>
   );
 }
