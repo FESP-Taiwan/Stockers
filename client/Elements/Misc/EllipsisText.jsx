be74dbe5378b007145e2903ed73011409e14c0ee
張丞逸 <parkerhiphop027@gmail.com>
2019-09-17 16:46:56 +0800

    add useMouseHover

---
+++ client/Elements/Misc/EllipsisText.jsx
@@ -0,0 +1,28 @@
+// @flow
+/* eslint react/no-array-index-key: 0 */
+
+import React, {
+  useState,
+  useMemo,
+  Fragment,
+} from 'react';
+import useMouseHover from '../../helper/useMouseHover';
+
+const styles = {
+
+};
+
+type Props = {
+
+};
+
+function EllipsisText() {
+  return (
+    <Fragment>
+      <div>
+      </div>
+    </Fragment>
+  );
+}
+
+export default EllipsisText;
