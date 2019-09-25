5a9122282017e7a19e554e9cdc3bc33254336f2a
yan <ian24257587@gmail.com>
2019-09-26 00:24:25 +0800

    add prevState feature

--- client/Elements/ArtiboxEditor/Editor.jsx
+++ client/Elements/ArtiboxEditor/Editor.jsx
@@ -2,23 +2,42 @@
 
 import React, {
   useReducer,
+  useRef,
+  useEffect,
 } from 'react';
 import reducer, { initializer } from './Reducer';
 import { fromJSON } from '../../helper/json';
 import { Dispatch as DispatchContext } from '../../Constant/ArtiboxEditor/context';
 
 const styles = {
-
+  wrapper: {
+    width: '100%',
+    height: '100%',
+  },
 };
+
+function usePreviosState(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  }, [value]);
+
+  return ref.current;
+}
 
 function Editor() {
   const [state, dispatch] = useReducer(reducer, fromJSON(), initializer);
+
+  const prevState = usePreviosState(state);
 
   console.log(state);
 
   return (
     <DispatchContext.Provider value={dispatch}>
+      <div style={styles.wrapper}>
 
+      </div>
     </DispatchContext.Provider>
   );
 }
