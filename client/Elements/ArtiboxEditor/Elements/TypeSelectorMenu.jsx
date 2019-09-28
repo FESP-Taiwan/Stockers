ce369e28bb938e3fabd6f51b5f41c57738cd5519
張丞逸 <parkerhiphop027@gmail.com>
2019-09-29 02:17:57 +0800

    add type menu block

--- client/Elements/ArtiboxEditor/Elements/TypeSelectorMenu.jsx
+++ client/Elements/ArtiboxEditor/Elements/TypeSelectorMenu.jsx
@@ -1,14 +1,27 @@
 // @flow
 
 import React from 'react';
+import Icons from '../../../Constant/ArtiboxEditor/icons';
 
 const styles = {
-
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    height: '100%',
+    flexGrow: 1,
+    backgroundColor: 'transparent',
+    border: 'solid 1px #000',
+  },
 };
 
 function TypeSelectorMenu() {
   return (
-    <div></div>
+    <div style={styles.wrapper}>
+      <Icons.TITLE />
+      <Icons.SUBTITLE />
+      <Icons.LINE />
+      <Icons.QUOTE />
+    </div>
   );
 }
 
