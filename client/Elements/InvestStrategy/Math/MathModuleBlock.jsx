87e7ae5bd1a687e5eee0bb6115bdbb724cdc02d5
yan <ian24257587@gmail.com>
2019-10-04 23:39:45 +0800

    set math module UI

---
+++ client/Elements/InvestStrategy/Math/MathModuleBlock.jsx
@@ -0,0 +1,55 @@
+// @flow
+
+import React from 'react';
+
+const styles = {
+  wrapper: {
+    width: '100%',
+    flexBasis: 100,
+    backgroundColor: Colors.PRIMARY_THIRD,
+    borderRadius: 40,
+    padding: '0 0 0 40px',
+    margin: '20px 0',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  header: {
+    fontSize: 14,
+    color: Colors.PRIMARY,
+    margin: '0 40px 0 0',
+    flexShrink: 0,
+  },
+  mathInputBlock: {
+    flexGrow: 1,
+  },
+  btn: {
+    flexBasis: 106,
+    height: '100%',
+    borderRadius: 40,
+    backgroundColor: Colors.PRIMARY_SECOND,
+    fontSize: 13,
+    lineHeight: '100px',
+    textAlign: 'center',
+  },
+};
+
+function MathModuleBlock() {
+  return (
+    <div style={styles.wrapper}>
+      <h2 style={styles.header}>數學公式：</h2>
+      <div style={styles.mathInputBlock}>
+
+      </div>
+      <button
+        onClick={() => {}}
+        style={styles.btn}
+        type="button">
+        註釋
+      </button>
+    </div>
+  );
+}
+
+export default MathModuleBlock;
---
+++ client/Elements/InvestStrategy/Math/MathModuleBlockWrapper.jsx
@@ -0,0 +1,12 @@
+// @flow
+
+import React from 'react';
+import MathModuleBlock from './MathModuleBlock';
+
+function MathModuleBlockWrapper() {
+  return (
+    <MathModuleBlock />
+  );
+}
+
+export default MathModuleBlockWrapper;
