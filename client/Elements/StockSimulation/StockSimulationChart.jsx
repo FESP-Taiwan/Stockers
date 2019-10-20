22b301843398ea538b82e87243df90fad7fa32a3
at7211 <at7211@gmail.com>
2019-10-20 14:08:10 +0800

    stockSimulation chart

---
+++ client/Elements/StockSimulation/StockSimulationChart.jsx
@@ -0,0 +1,23 @@
+// @flow
+/** @jsx jsx */
+
+import { jsx } from '@emotion/core';
+
+const styles = {
+  wrapper: {
+    backgroundColor: Colors.LAYER_FIRST,
+    width: '100%',
+    height: 600,
+    borderRadius: 20,
+  },
+};
+
+function StockSimulationCahrt() {
+  return (
+    <div css={styles.wrapper}>
+      (⁎⁍̴̛ᴗ⁍̴̛⁎)
+    </div>
+  );
+}
+
+export default StockSimulationCahrt;
