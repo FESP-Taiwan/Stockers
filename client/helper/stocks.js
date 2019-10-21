071ef3344a51aadfa400f587fdbaa1ad128c3b9f
yan <ian24257587@gmail.com>
2019-10-21 21:46:56 +0800

    add formatDateValue feature

--- client/helper/stocks.js
+++ client/helper/stocks.js
@@ -1,13 +1,27 @@
 // @flow
 
+import moment from 'moment';
+
+function formatDateValue(value) {
+  const formatChars = ['年', '月', '日'];
+
+  const charsPostitions = formatChars.map(char => value.indexOf(char));
+
+  const formatedDate = charsPostitions.reduce((formatingValue, pos) => {
+    if (!formatingValue.substring(pos + 1)) {
+      return `${formatingValue.substring(0, pos)}`;
+    }
+
+    return `${formatingValue.substring(0, pos)}-${formatingValue.substring(pos + 1)}`;
+  }, value);
+
+  return moment(formatedDate);
+}
+
 export function prettifyStockData(data) {
   console.log('data', data);
 
-  const balanceSheet = {
-    name: '資產負債表',
-  };
-
-  const balanceSheetChips = data.BalanceSheet.reduce((set, el) => {
+  const balanceSheetChipsSet = data.BalanceSheet.reduce((set, el) => {
     const chipNames = Object.keys(el);
 
     chipNames.forEach(chipName => set.add(chipName));
@@ -15,7 +29,20 @@ export function prettifyStockData(data) {
     return set;
   }, new Set());
 
-  console.log('balanceSheetChips', balanceSheetChips);
+  const balanceSheetChips = Array.from(balanceSheetChipsSet);
+
+  const balanceSheet = {
+    name: '資產負債表',
+    chipInfos: balanceSheetChips.map(chip => ({
+      chipName: chip,
+      chipData: data.BalanceSheet.map(info => ({
+        date: formatDateValue(info.report_date),
+        value: info[chip],
+      })),
+    })),
+  };
+
+  console.log('balanceSheet', balanceSheet);
 
   return data;
 }
