847b9410af8d870c11a4294c64100e9ace9b9f46
yan <ian24257587@gmail.com>
2019-10-27 10:40:26 +0800

    add confirm modal

--- client/Elements/InvestStrategy/CommentBlock.jsx
+++ client/Elements/InvestStrategy/CommentBlock.jsx
@@ -16,6 +16,7 @@ import {
   EDITTER_GET_GRID,
 } from '../../Constant/investStrategy';
 import { FIXED_BUTTON_INDEX, BASE_CONTAINER_INDEX } from '../../Constant/zIndex';
+import Modal from '../Modal/Modal';
 
 const styles = {
   wrapper: {
@@ -49,7 +50,6 @@ const styles = {
     height: 0,
     borderRadius: 40,
     backgroundColor: Colors.LAYER_THIRD,
-    // transition: '0.5s',
     opacity: 0,
   },
   formBlockActived: {
@@ -57,12 +57,59 @@ const styles = {
     height: 456,
     opacity: 1,
   },
+  modalWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    width: 228,
+    height: 216,
+    position: 'relative',
+  },
+  header: {
+    fontSize: 22,
+    letterSpacing: 2,
+    color: Colors.PRIMARY,
+    margin: '0 0 16px 0',
+  },
+  p: {
+    fontSize: 14,
+    margin: 0,
+  },
+  btnWrapper: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+  cancelBtn: {
+    width: 72,
+    height: 40,
+    borderRadius: 4,
+    lineHeight: '32px',
+    textAlign: 'center',
+    backgroundColor: Colors.ERROR,
+    fontSize: 14,
+    margin: '0 8px',
+  },
+  acceptBtn: {
+    width: 72,
+    height: 40,
+    borderRadius: 4,
+    lineHeight: '32px',
+    textAlign: 'center',
+    backgroundColor: Colors.PRIMARY,
+    fontSize: 14,
+    margin: '0 8px',
+  },
 };
 
 function CommentBlock() {
   const [isFormOpened, setFormOpened] = useState(false);
   const [gridInfoForEmitTrigger, setGridInfoForEmitTrigger] = useState({});
   const [isMathModuleEditting, setMathModuleEditting] = useState(false);
+  const [isConfirmedModalOpened, setConfirmedModalOpened] = useState(false);
 
   useEffect(() => {
     function mathStartEditHandler() {
@@ -105,7 +152,11 @@ function CommentBlock() {
   }, [gridInfoForEmitTrigger]);
 
   const onClick = useCallback(() => {
-    setFormOpened(!isFormOpened);
+    if (!isFormOpened) {
+      setFormOpened(true);
+    } else {
+      setConfirmedModalOpened(true);
+    }
   }, [isFormOpened]);
 
   const editor = useMemo(() => {
@@ -115,6 +166,36 @@ function CommentBlock() {
       <Editor />
     );
   }, [isFormOpened]);
+
+  const confirmedModal = useMemo(() => {
+    if (!isConfirmedModalOpened) return null;
+
+    return (
+      <Modal>
+        <div style={styles.modalWrapper}>
+          <h2 style={styles.header}>確認是否關閉</h2>
+          <p style={styles.p}>關閉模組將遺失修改內容，請儲存後再關閉</p>
+          <div style={styles.btnWrapper}>
+            <button
+              style={styles.cancelBtn}
+              onClick={() => setConfirmedModalOpened(false)}
+              type="button">
+              取消
+            </button>
+            <button
+              style={styles.acceptBtn}
+              onClick={() => {
+                setConfirmedModalOpened(false);
+                setFormOpened(false);
+              }}
+              type="button">
+              關閉
+            </button>
+          </div>
+        </div>
+      </Modal>
+    );
+  }, [isConfirmedModalOpened]);
 
   const formBlockStyles = useMemo(() => ({
     ...styles.formBlock,
@@ -132,6 +213,7 @@ function CommentBlock() {
       <div style={formBlockStyles}>
         {editor}
       </div>
+      {confirmedModal}
     </div>
   );
 }
