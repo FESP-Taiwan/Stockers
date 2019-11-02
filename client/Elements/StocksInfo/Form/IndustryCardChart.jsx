0f3287631743f44602c02fb6a3ae5eb8906c624e
at7211 <at7211@gmail.com>
2019-11-02 18:59:46 +0800

    add YAxis

--- client/Elements/StocksInfo/Form/IndustryCardChart.jsx
+++ client/Elements/StocksInfo/Form/IndustryCardChart.jsx
@@ -3,7 +3,7 @@
 import React, { useMemo } from 'react';
 // 引用要使用的圖表元素
 import {
-  AreaChart, Area,
+  AreaChart, Area, YAxis,
 } from 'recharts';
 
 const styles = {
@@ -31,12 +31,12 @@ function IndustryCardChart({
   const area = useMemo(() => {
     if (average < 0) {
       return (
-        <Area type="monotone" dataKey="percent" stakeId="1" stroke="#5EF28F" fill="#5EF28F" />
+        <Area type="monotone" dataKey="percent" stakeId="1" stroke="#5EF28F" fill="#5EF28F" unit={100} />
       );
     }
 
     return (
-      <Area type="monotone" dataKey="percent" stakeId="1" stroke="#FF7E72" fill="#FF7E72" />
+      <Area type="monotone" dataKey="percent" stakeId="1" stroke="#FF7E72" fill="#FF7E72" unit={100} />
     );
   }, [average]);
 
@@ -44,6 +44,7 @@ function IndustryCardChart({
     <div style={styles.wrapper}>
       <AreaChart width={235} height={120} data={data}>
         {area}
+        <YAxis hide type="number" domain={['dataMin', 'dataMax']} />
       </AreaChart>
     </div>
   );
